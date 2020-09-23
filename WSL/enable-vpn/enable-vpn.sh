ef10550409bd3010729b632e4e0ac19c3a2d6a1d
Lino Marcos <linomarcos@gmail.com>
2020-09-23 10:00:05 -0300

    Update enable-vpn.sh

--- WSL/enable-vpn/enable-vpn.sh
+++ WSL/enable-vpn/enable-vpn.sh
@@ -2,6 +2,6 @@ read -p 'Você está conectado na VPN atraves do Cysco Anyconnect? Digite "sim" 
 if [ "$proxy_indicator" = "sim" ]; then
    /mnt/c/Windows/System32/cmd.exe /c start "c:\enable-vpn.ps1.lnk"
    #/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe Start-Proccess -FilePath Start-Process "C:\enable-vpn.ps1.lnk"
-   cp /etc/resolv.conf.vpn /etc/resolv.conf
+   #cp /etc/resolv.conf.vpn /etc/resolv.conf
    echo 'Ativado o suporte de proxy no WSL'
 fi
